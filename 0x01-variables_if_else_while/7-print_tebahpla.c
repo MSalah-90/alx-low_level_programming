8e9516b2d1ebc7fa04672473549fbcb2bbd4a5ea
Michael Salah <michael.salah@hotmail.com>
2023-06-16 16:00:38 -0700

    print alphabet reversly

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all alphabet small letters reversli
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
