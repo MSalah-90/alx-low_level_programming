ca4484b94102e2d5e2a17315b3098f37bdc46800
Michael Salah <michael.salah@hotmail.com>
2023-06-21 13:46:17 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * @n: is the input
+ * Description: prints last digit of a number
+ *
+ * Return: 0 (success)
+*/
+
+int print_last_digit(int n)
+{
+	int x = n;
+
+	if (x < 0)
+		x = -1 * (x % 10);
+	else 
+		x = x % 10;
+
+	_putchar(x + 48);
+	return (x);
+}
