e147b4216e26a7e929e9c2e0c73eb151e6c2d352
Michael Salah <michael.salah@hotmail.com>
2023-06-21 14:15:43 -0700

    minutes of day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - entry point
+ *
+ * Description: prints every minute of the day
+ *
+ * Return: 0 (success)
+*/
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 3; a++)
+	{
+
+		for (b = 0; b < 4; b++)
+		{
+
+			for (c = 0; c < 6; c++)
+			{
+
+				for (d = 0; d < 10; d++)
+				{
+
+				_putchar(a + 48);
+				_putchar(b + 48);
+				_putchar(':');
+				_putchar(c + 48);
+				_putchar(d + 48);
+				_putchar('\n');
+				}
+
+
+
+			}
+
+
+		}
+
+
+	}
+}
