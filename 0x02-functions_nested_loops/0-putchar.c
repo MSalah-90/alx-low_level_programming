ce2bc50179016fbe00be7853738bbae5a2de265c
Michael Salah <michael.salah@hotmail.com>
2023-06-20 21:31:20 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints putchar.
+ *
+ * Return: 0 (success)
+ *
+*/
+
+
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
