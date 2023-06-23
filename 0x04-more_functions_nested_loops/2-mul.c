70fba94f77ff6cf58117dce893759a3781c74cc2
Michael Salah <michael.salah@hotmail.com>
2023-06-23 13:17:24 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul -  Entry - point
+ *
+ * @a: is the input num one
+ * @b: is the input num two
+ *
+ * Describtion: mul returns product of two num
+ *
+ * Return: product
+*/
+
+int mul(int a, int b)
+{
+		return (a * b);
+}
