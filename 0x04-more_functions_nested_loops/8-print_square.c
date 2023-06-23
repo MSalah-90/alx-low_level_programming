4892da5c94fa47320b88893bd0e341ff25c533ca
Michael Salah <michael.salah@hotmail.com>
2023-06-23 16:14:54 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,18 +13,18 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size > 0)
-	{
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}	
+	if (size <= 0)
 		_putchar('\n');
-		}
-
-	}
 	else
+	{
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar('#');
+		}	
 		_putchar('\n');
+	}
+	}
 }
