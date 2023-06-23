5fbb36ff24541fa0eb9a28602275e570cb6b2cde
Michael Salah <michael.salah@hotmail.com>
2023-06-23 13:50:58 -0700

    more nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers -  Entry - point
+ *
+ * Describtion: more_numbers prints nums from 0 to 14 ten times
+ *
+ * Return: Nothing (success)
+*/
+
+void more_numbers(void)
+{
+		int i, j;
+
+		for (i = 0; i < 10; i++)
+		{
+			for (j = 0; j < 15; j++)
+			{
+				num = j;
+
+				if (j > 9)
+				{
+					_putchar(1 + 48);
+					num = j % 10;
+				}
+				_putchar(num + 48);
+			}
+		}
+		_putchar('\n');
+}
