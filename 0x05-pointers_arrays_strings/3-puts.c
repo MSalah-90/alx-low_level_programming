bbccf097d5a1abea3e1c2524ce6044bb30ac9af8
Michael Salah <michael.salah@hotmail.com>
2023-06-26 11:38:07 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <math.h>
+
+/**
+ * _puts - entry point
+ * @str: is the char input
+ *
+ * Description: prints string of char
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != 0)
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
