22200748d43ca6c18c1917bc31d2a74ca1d97dac
Michael Salah <michael.salah@hotmail.com>
2023-06-27 14:26:36 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <math.h>
+
+/**
+ * print_array - entry point
+ * @a: is the int input array
+ * @n: is the size of array
+ *
+ * Description: prints n elements of an array of integers
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int j = 0;
+
+	while (j < n - 1)
+	{
+		printf("%d, ", a[j]);
+		j += 1;
+	}
+	printf("%d", a[n - 1]);
+	printf("\n");
+}
+
