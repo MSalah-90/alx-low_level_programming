c152323baf25376a876071b0046b3f3443a0a4dd
Michael Salah <michael.salah@hotmail.com>
2023-06-27 14:00:51 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <math.h>
+
+/**
+ * puts_half - entry point
+ * @str: is the char input
+ *
+ * Description: prints second half of a string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; j <= i; j++)
+		{
+			_putchar(str[j] + 0);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (j = (i - 1) / 2; j <= i; j++)
+		{
+			_putchar(str[j] + 0);
+		}
+		_putchar('\n');
+
+	}
+}
