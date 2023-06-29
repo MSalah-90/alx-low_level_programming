85c1a5e15147f1d34428bce981ea242838f34620
Michael Salah <michael.salah@hotmail.com>
2023-06-29 12:02:27 -0700

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * *string_toupper - Entry Point
+ *
+ * @n: name of array
+ *
+ * Description: upper case each element of array
+ *
+ * Return: Nothing
+ *
+*/
+
+char *string_toupper(char *n)
+{
+	int i, j;
+
+	i = 0;
+	while (n[i])
+		i++;
+	
+	for (j = 0; j < i; j++)
+	{
+		if (n[j] > 96 && n[j] < 123)
+		n[j] -= 32;
+	}
+	return (n);
+}
