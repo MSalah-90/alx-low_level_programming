692e337a54011268005165fbeee1a5edabb2a714
Michael Salah <michael.salah@hotmail.com>
2023-07-04 13:59:26 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_chessboard - Entry Point
+ * @a: array of chars
+ *
+ *
+ * Description: function prints the chessboard
+ *
+ *
+ * Return Nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	long unsigned int i, j;
+
+	i = 0;
+	for (; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n');
+	}
+}
