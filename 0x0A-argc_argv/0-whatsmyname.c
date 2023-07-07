4fe8bfd03b331106cb8cbd72701fbc9376029e3e
Michael Salah <michael.salah@hotmail.com>
2023-07-07 13:37:58 -0700

    mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - func entry point
+ * @argc: counter
+ * @argv: string of chars
+ *
+ * Description: returns name
+ *
+ * Return: int
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+return (0);
+}
