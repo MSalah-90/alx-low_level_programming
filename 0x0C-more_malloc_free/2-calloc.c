09e5396ea32b6c56330ebc7ce7e92a803a86f5c2
Michael Salah <michael.salah@hotmail.com>
2023-07-14 14:01:17 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _calloc - entry point
+ * @nmemb: size of array
+ * @size: size of type
+ *
+ * Description:  function allocates memory for an array of nmemb elements of
+ * size bytes each and returns a pointer to the allocated memory.
+ *
+ * Return: The memory is set to zero. If nmemb or size is 0, then _calloc
+ * returns NULL. If malloc fails, then _calloc returns NULL
+ *
+ */
+
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *var;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	var = malloc(size * nmemb + 1);
+
+	if (var == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i <= nmemb)
+	{
+		var[i] = 0;
+		i++;
+	}
+	var[i] = '\0';
+	return (var);
+}
