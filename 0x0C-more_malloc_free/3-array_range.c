e7c47d214fc3b470450f9ad2437b2ac7b128c861
Michael Salah <michael.salah@hotmail.com>
2023-07-14 14:49:01 -0700

    array rng

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * array_range - entry point
+ * @min: min int
+ * @max: max int
+ *
+ * Description: array created should contain all the values
+ * from min (included) to max (included), ordered from min to max
+ *
+ * Return: the pointer to the newly created array
+ * If min > max, return NULL
+ * If malloc fails, return NULL
+ *
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int t;
+	int *var;
+
+	if (min > max)
+		return (NULL);
+
+	var = malloc(sizeof(int) * max - min + 1);
+
+	if (var == NULL)
+		return (NULL);
+
+	i = 0;
+	t = min;
+	while (i < max - min + 1)
+	{
+		var[i] = t;
+		i++;
+		t++;
+	}
+	return (var);
+}
