e568428cf2c54c00356457b12e55c11cb5919944
Michael Salah <michael.salah@hotmail.com>
2023-07-14 11:28:31 -0700

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <string.h>
 
 /**
  * string_nconcat - entry point
@@ -28,17 +29,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL && s2 != NULL)
 	{
 		s1 = "";
-		var = malloc(sizeof(char) * (sizeof(s2)));
+		var = malloc(sizeof(char) * (strlen(s2) + 1));
 	}
 	if (s2 == NULL && s1 != NULL)
 	{
 		s2 = "";
-		var = malloc(sizeof(char) * (sizeof(s1)));
+		var = malloc(sizeof(char) * (strlen(s1) + 1));
 	}
 		if (n < sizeof(s2))
-			var = malloc(sizeof(char) * ((sizeof(s1) + n)));
+			var = malloc(sizeof(char) * ((strlen(s1) + n + 1)));
 		else if (n >= sizeof(s2))
-			var = malloc(sizeof(char) * (sizeof(s1) + sizeof(s2) - 1));
+			var = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 
 	if (var == NULL)
 		return (NULL);
