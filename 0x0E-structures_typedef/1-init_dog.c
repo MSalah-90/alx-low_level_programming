2e43e5125a5792bafca7b93b5c95f46eda7737a0
Michael Salah <michael.salah@hotmail.com>
2023-07-19 11:33:14 -0700

    ok

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - entry point
+ * @d: struct dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: whole structure
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
