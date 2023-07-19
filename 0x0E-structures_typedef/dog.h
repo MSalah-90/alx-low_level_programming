bfa1c6b132f52560d76d7cd57b21e53212ba13a0
Michael Salah <michael.salah@hotmail.com>
2023-07-19 11:18:19 -0700

    ok

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - main struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: new struct for dog details
+ */
+
+
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
