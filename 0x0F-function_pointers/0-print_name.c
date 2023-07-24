194c49c4e918e64f726ef907d8dab493074c2eae
Michael Salah <michael.salah@hotmail.com>
2023-07-24 13:22:38 -0700

    ok

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: func
+ *
+ * Description: pointer to function
+ *
+ * Return: Nothing.
+ *
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+		f(name);
+}
